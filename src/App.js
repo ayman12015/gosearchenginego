fd8045d1e3054a2ef124846ba5fa0b00f7567fe9
ayman22 <ayman@digitalorder.net>
2022-01-28 03:02:06 +0300

    log

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+
+function App() {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+  const [searchInfo, setSearchInfo] = useState({});
+  const handleSearch = async e => {
+    e.preventDefault();
+    if (search === '') return;
+
+    const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${search}`
+    const response = await fetch(endpoint);
+
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+
+    const json = await response.json();
+    // console.log(json.query.searchinfo);
+    setResults(json.query.search);
+    setSearchInfo(json.query.searchinfo);
+  }
+  return (
+    <div className="App">
+      <header>
+        <h1>Go <strong>Engine</strong></h1>
+        <form className="search-box" onSubmit={handleSearch}>
+          <input
+            type="search"
+            placeholder="what are you looking for?.."
+            value={search}
+            onChange={e => setSearch(e.target.value)}
+          />
+        </form>
+        {(searchInfo.totalhits) ? <p>عدد مرات البحث  :  {searchInfo.totalhits}</p> : ''}
+      </header>
+
+      <div className="results">
+        {results.map((result, i) => {
+          const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
+          return (
+            <div className="result" key={i}>
+              <h3>{result.title}</h3>
+              <p dangerouslySetInnerHTML={{  __html: result.snippet}}></p>
+              <a href={url} target="_blank" rel='nofollow'>Read More</a>
+            </div>
+          )
+        })}
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
